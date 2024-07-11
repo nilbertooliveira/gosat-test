58c29dd3b3f647f5597d0a9b63f577bfe9b22317
Nilberto Oliveira <nilbertooliveira.ti@gmail.com>
2024-07-11 19:44:56 -0300

    Chart

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,6 +24,9 @@
                 <a class="navbar-brand" href="{{ route('simulation.chart') }}">
                     {{ __('Gráfico') }}
                 </a>
+                <a class="navbar-brand" href="{{ route('simulation.credit-view') }}">
+                    {{ __('Simulador') }}
+                </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
